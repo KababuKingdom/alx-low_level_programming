16aec382eff016a7bbce895f1059603b80d56425
KababuKingdom <kababukingdom@gmail.com>
2022-09-12 19:44:02 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
-* main - Printing
+* main – Entry point
 *
-* Return: Always 0 (Successful)
+* Return: 0 after printing the function
 */
 int main(void)
 {
-puts("\"Programming is like building a multilingual
-puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
