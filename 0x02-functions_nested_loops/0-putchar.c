80961c075d562673cc33c5e9283502a2cbb75336
KababuKingdom <kababukingdom@gmail.com>
2022-09-23 09:42:52 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,31 @@
-#include "main.h";
+#include "main.h"
 
 /**
- *main - prints putchar
- *Return: always0
- */
-int main (void)
-{
-  _putchar('_');
-  _putchar('p');
-  _putchar('u');
-  _putchar('t');
-  _putchar('c');
-  _putchar('h');
-  _putchar('a');
-  _putchar('r');
-  _putchar('\n');
+* main - print putchar
+* Return: always 0
+*/
+int main(void)
 
-  Return (0) ;
+{
+
+_putchar('_');
+
+_putchar('p');
+
+_putchar('u');
+
+_putchar('t');
+
+_putchar('c');
+
+_putchar('h');
+
+_putchar('a');
+
+_putchar('r');
+
+_putchar('\n');
+
+return (0);
 
 }
