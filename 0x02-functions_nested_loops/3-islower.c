5818e6824e83f577aad6d0c9555a7fc7fcc506e7
KababuKingdom <kababukingdom@gmail.com>
2022-09-23 13:32:42 -0700

    Task 1-4

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,32 +1,15 @@
-#include “main.h”
+#include "main.h"
 /**
- * _islower – function to check for lowercase character
+ * _islower - function to check for lowercase character
  * @c:  is the int that will use for the argument of the function
  * Return: 0
  */
-Int _islower(int c)
+int _islower(int c)
 {
-	If (c >= ‘a’ && c <= ‘z’)
+	if (c >= 'a' && c <= 'z')
 	{
-		Return (1);
+		return (1);
 	}
-	Else
-		Return (0);
-}
-
-Task 4 copy below 
-#include “main.h”
-/**
- * _isalpha – function to check if c is a letter, lowercase or uppercase
- * @c: is the int that will use for the argument of the function
- * Return: 0
- */
-Int _isalpha(int c)
-{
-	If ((c >= ‘a’ && c <= ‘z’) || (c >= ‘A’ && c <= ‘Z’))
-	{
-	Return (1);
-	}
-	Else
-	Return (0);
+	else
+		return (0);
 }
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,15 +1,15 @@
-#include “main.h”
+#include "main.h"
 /**
- * _isalpha – function to check if c is a letter, lowercase or uppercase
+ * _isalpha - function to check if c is a letter, lowercase or uppercase
  * @c: is the int that will use for the argument of the function
  * Return: 0
  */
-Int _isalpha(int c)
+int _isalpha(int c)
 {
-	If ((c >= ‘a’ && c <= ‘z’) || (c >= ‘A’ && c <= ‘Z’))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
-	Return (1);
+	return (1);
 	}
-	Else
-	Return (0);
+	else
+	return (0);
 }
